4de925d337fdc5fa0214304dce5f1b1eb113220e
Warren Voelkl <warrenvoelkl@gmail.com>
2019-07-05 11:08:23 -0700

    Not working right yet

--- levenshtein-distince-between-two-strings/src/main/java/com/wvoelkl/Solution.java
+++ levenshtein-distince-between-two-strings/src/main/java/com/wvoelkl/Solution.java
@@ -13,6 +13,29 @@ public class Solution {
 
 
     public int minDistance(String word1, String word2) {
-        return 0;
+
+        //Never will be greater than max distance
+        int[][] distanceMatrix = new int[word1.length()][word2.length()];
+
+        for (int i = 1; i < word1.length(); i++)
+            distanceMatrix[i][0] = i;
+
+        for (int j = 1; j < word2.length(); j++)
+            distanceMatrix[0][j] = j;
+
+        for (int i = 1; i < word2.length() - 1; i++) {
+            for (int j = 1; j < word1.length() - 1; j++) {
+                int subCost = (word1.charAt(i) == word2.charAt(j)) ? 0 : 1;
+
+                int a = distanceMatrix[i - 1][j] + 1;               //delete
+                int b = distanceMatrix[i][j - 1] + 1;               //create
+                int c = distanceMatrix[i - 1][j - 1] + subCost;     //update
+
+                distanceMatrix[i][j] = Math.min(a, Math.min(b, c));
+            }
+
+
+        }
+        return distanceMatrix[word1.length() - 1][word2.length() - 1];
     }
 }
